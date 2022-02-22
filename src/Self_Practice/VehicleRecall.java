f058a38538698ca4f45a3632d309a2995988a4f7
Emanuil <emanuil.karakashev@gmail.com>
2022-02-21 20:28:08 -0500

    Combined years with () and separate different time frames with ||.

--- src/Self_Practice/VehicleRecall.java
+++ src/Self_Practice/VehicleRecall.java
@@ -14,8 +14,7 @@ public class VehicleRecall {
 
             //WRITE YOUR CODE BELOW:
 
-            if((vehicleYear >=1995 && vehicleYear <=1998) || (vehicleYear >= 2001 && vehicleYear <= 2002)
-                    || (vehicleYear >= 2004 && vehicleYear <= 2006) || (vehicleYear >=2015 && vehicleYear <=2017)){
+            if((vehicleYear >=1995 && vehicleYear <=1998) || (vehicleYear >= 2001 && vehicleYear <= 2002) || (vehicleYear >= 2004 && vehicleYear <= 2006) || (vehicleYear >=2015 && vehicleYear <=2017)){
                 System.out.println("Your vehicle needs to be recalled!");
             }else{
                 System.out.println("Your vehicle is fine, enjoy!");
